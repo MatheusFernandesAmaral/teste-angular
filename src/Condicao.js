2c568c234a4f98acc37bced3668024ce23dea16b
MatheusFernandesAmaral <114800601+MatheusFernandesAmaral@users.noreply.github.com>
2022-10-06 23:08:35 -0300

    2 files modified, 2 added

---
+++ src/Condicao.js
@@ -0,0 +1,13 @@
+var resposta = "Sim";
+
+if (resposta = "Sim") {
+    <a href = "https://633f75de0dbc3309f3cae9dc.mockapi.io/api/v1/Res"> </a>;   
+
+} else {
+    <a href = "https://633f75de0dbc3309f3cae9dc.mockapi.io/api/v1/respn "> a</a>;   
+}
+
+
+function nome_funcao() {
+  alert("Login ou senha incorreta!");
+}
---
+++ src/app/if.php
@@ -0,0 +1,11 @@
+
+<?php
+$t = date("10");
+
+if ($t < "20") {
+  echo "Have a good day!";
+} else {
+  echo "Have a good night!";
+}
+?>
+ 
